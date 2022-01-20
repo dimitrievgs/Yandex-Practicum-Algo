cccbb8ed2fe273fb98c2a97eaac8bfccf1f7332b
dimitrievgs <dimitriev.gs@mail.ru>
2022-01-20 21:56:41 +0300

    +

---
+++ S2/S2TJ-Списочная очередь.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yandex_Praktikum_Algo.S2
+{
+    class S2TJ_Списочная_очередь
+    {
+    }
+}

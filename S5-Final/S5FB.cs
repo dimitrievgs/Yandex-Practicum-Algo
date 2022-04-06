69d364ba2d9c9b9897905eb4323383163f6b2e2d
dimitrievgs <dimitriev.gs@mail.ru>
2022-04-06 23:33:06 +0300

    S5FB - in process

--- S5-Final/S5FB.cs
+++ S5-Final/S5FB.cs
@@ -10,7 +10,80 @@ namespace S5FB
             Test();
         }
 
-        public static Node Insert(Node root, int key)
+        public static Node Remove(Node root, int key)
+        {
+            // Your code
+            // “ヽ(´▽｀)ノ”
+
+            Node current = root;
+            Node currentParent = null;
+            bool currentIsLeftChild = false;
+            while (current.Value != key)
+            {
+                if (key < current.Value)
+                {
+                    currentParent = current;
+                    current = current.Left;
+                    currentIsLeftChild = true;
+                }
+                else if (key > current.Value)
+                {
+                    currentParent = current;
+                    current = current.Right;
+                    currentIsLeftChild = false;
+                }
+                if (current == null) //нет такого узла
+                    return null;
+            }
+            //сюда попадаем, если нашли узел: current.Value == key
+            if (current.Left == null && current.Right == null) //нет детей у узла, т.е. лист
+            {
+                if (!currentIsLeftChild)
+                    currentParent.Right = null;
+                else
+                    currentParent.Left = null;
+                //remove the edge from current.parent to current
+            }
+            else if (current.Left == null || current.Right == null) //у узла 1 дочерний
+            {
+                bool hasLeftChild = current.Left != null;
+                if (!currentIsLeftChild)
+                    currentParent.Right = hasLeftChild ? current.Left : current.Right;
+                else
+                    currentParent.Left = hasLeftChild ? current.Left : current.Right;
+                //have current.parent point to current’s child instead of to current
+            }
+            else //у узла 2 дочерних
+            {
+                Node successor = null;
+                if (successor == )
+            }
+
+        }
+
+        /// <summary>
+        /// Функция преемника (successor) узла бинарного дерева поиска
+        /// находит «преемника» — следующего по величине узла в BST.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        private static Node Successor(Node node)
+        {
+            Node current = null;
+            if (node.Right != null) //случай 1: есть дочерний узел справа
+            {
+                current = node.Right;
+                while (current.Left != null)
+                    current = current.Left;
+                return current;
+            }
+            else //случай 2: node не имеет дочернего узла справа
+            {
+                
+            }
+        }
+
+        /*public static Node Insert(Node root, int key)
         {
             InsertNode(root, key);
             return root;
@@ -38,23 +111,43 @@ namespace S5FB
                     root.Left = node;
                 }
             }
-        }
+        }*/
 
-        private static void Test()
+        public static void Test()
         {
-            var node1 = new Node(7);
-            var node2 = new Node(8)
+            var node1 = new Node(2);
+            var node2 = new Node(3)
             {
                 Left = node1
             };
 
-            var node3 = new Node(7)
+            var node3 = new Node(1)
             {
                 Right = node2
             };
-            var newHead = Insert(node3, 6);
-            Console.WriteLine(newHead == node3);
-            Console.WriteLine(newHead.Left.Value == 6);
+
+            var node4 = new Node(6);
+            var node5 = new Node(8)
+            {
+                Left = node4
+            };
+
+            var node6 = new Node(10)
+            {
+                Left = node5
+            };
+
+            var node7 = new Node(5)
+            {
+                Left = node3,
+                Right = node6
+            };
+
+            var newHead = Remove(node7, 10);
+
+            Console.WriteLine(newHead.Value == 5);
+            Console.WriteLine(newHead.Right == node5);
+            Console.WriteLine(newHead.Right.Value == 8);
         }
     }
 

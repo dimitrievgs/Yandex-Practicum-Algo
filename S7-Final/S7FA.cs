1272fb5c4fc3001107ee26d0a70acb9cf04d97a5
dimitrievgs <dimitriev.gs@mail.ru>
2022-05-03 21:59:12 +0300

    S7FA, S7FB

--- S7-Final/S7FA.cs
+++ S7-Final/S7FA.cs
@@ -45,32 +45,32 @@ namespace S7FA
             //значения по столбцам j от 0 до M соответствуют рассмотрению части строки sTwo длины j
             //например, в ячейке dp[i, j] будет мин-е кол-во операций (вставка, удаление, замена),
             //чтобы превратить sOne.Substring(0, i) в sTwo.Substring(0, j)
-            int N = sOne.Length;
-            int M = sTwo.Length;
+            int n = sOne.Length;
+            int m = sTwo.Length;
             //в массив N x M вводим дополнительный ряд снизу (i = 0) и слева (j = 0)
             //значения там - базовый случай: одна из строк пустая, или обе пустые
-            int[,] dp = new int[N + 1, M + 1];
+            int[,] dp = new int[n + 1, m + 1];
 
             //заносим значения для базового случая
             dp[0, 0] = 0; //i == 0 && j == 0
-            for (int i = 1; i <= N; i++) //j == 0 && i > 0
+            for (int i = 1; i <= n; i++) //j == 0 && i > 0
                 dp[i, 0] = i;
-            for (int j = 1; j <= M; j++) //i == 0 && j > 0
+            for (int j = 1; j <= m; j++) //i == 0 && j > 0
                 dp[0, j] = j;
 
             //теперь переход динамики: считаем для i > 0, j > 0, т.е., когда обе подстроки не пусты
-            for (int i = 1; i <= N; i++)
+            for (int i = 1; i <= n; i++)
             {
-                for (int j = 1; j <= M; j++)
+                for (int j = 1; j <= m; j++)
                 {
                     int iStep = dp[i - 1, j] + 1; //удаление из sOne
                     int jStep = dp[i, j - 1] + 1; //вставка в sOne
-                    int m = (sOne[i - 1] == sTwo[j - 1]) ? 0 : 1;
-                    int ijStep = dp[i - 1, j - 1] + m; //замена символа или отсутствие изменений
+                    int charChangeOrNot = (sOne[i - 1] == sTwo[j - 1]) ? 0 : 1;
+                    int ijStep = dp[i - 1, j - 1] + charChangeOrNot; //замена символа или отсутствие изменений
                     dp[i, j] = Math.Min(Math.Min(iStep, jStep), ijStep); //сравнение с элементами <-, ^, <- и ^ по массиву dp
                 }
             }
-            return dp[N, M];
+            return dp[n, m];
         }
     }
 }
